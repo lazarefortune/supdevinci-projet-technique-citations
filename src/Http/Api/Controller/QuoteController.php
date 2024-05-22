8b94e21c37e0410b2c9b2bc27c7e8cdaf5d70f35
Lazare Fortune <lazarefortune@gmail.com>
2024-05-22 21:44:15 +0200

    chore: refactor quote controller

--- src/Http/Api/Controller/QuoteController.php
+++ src/Http/Api/Controller/QuoteController.php
@@ -14,151 +14,133 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-#[Route( '/api/quotes' )]
+#[Route('/api/quotes')]
 class QuoteController extends AbstractController
 {
-//    #[Route('/', name: 'quotes')]
-//    public function index(QuoteRepository $quoteRepository): Response
-//    {
-//        $quotes = $quoteRepository->findAll();
-//
-//        return $this->json([
-//            'quotes' => $quotes
-//        ]);
-//    }
 
-    #[Route( '/', name: 'api_quotes', methods: ['GET'] )]
-    public function getQuotes( Request $request, QuoteRepository $quoteRepository ) : JsonResponse
+    public function __construct(private readonly EntityManagerInterface $em)
     {
-        $quotes = $quoteRepository->findBy( [], ['likes' => 'DESC', 'dislikes' => 'ASC'] );
-        return $this->json( ['quotes' => $quotes], 200, [], ['groups' => 'quote:read'] );
     }
 
-    #[Route( '/', name: 'api_create_quote', methods: ['POST'] )]
-    public function createQuote( Request $request, EntityManagerInterface $em ) : JsonResponse
+    #[Route('/', name: 'api_quotes', methods: ['GET'])]
+    public function getQuotes(QuoteRepository $quoteRepository): JsonResponse
     {
-        $data = json_decode( $request->getContent(), true );
-
-        // check required fields
-        if ( !isset( $data['content'] ) || !isset( $data['author'] ) ) {
-            return $this->json( ['error' => 'Missing required fields (content, author)'], 400 );
-        }
-
-        $quote = new Quote();
-        $quote->setContent( $data['content'] );
-        $quote->setAuthor( $data['author'] );
-        if ( isset( $data['isVerified'] ) ) {
-            $quote->setIsVerified( $data['isVerified'] );
-        }
-        $em->persist( $quote );
-        $em->flush();
-        return $this->json( $quote, 201, [], ['groups' => 'quote:read'] );
+        $quotes = $quoteRepository->findBy([], ['likes' => 'DESC', 'dislikes' => 'ASC']);
+        return $this->json(['quotes' => $quotes], Response::HTTP_OK, [], ['groups' => 'quote:read']);
     }
 
-    #[Route( '/{id}', name: 'api_update_quote', methods: ['PUT'] )]
-    public function updateQuote( $id, Request $request, EntityManagerInterface $em, QuoteRepository $quoteRepository ) : JsonResponse
+    #[Route('/', name: 'api_create_quote', methods: ['POST'])]
+    public function createQuote(Request $request): JsonResponse
     {
-        $quote = $quoteRepository->find( $id );
-        if ( !$quote ) {
-            return $this->json( ['error' => 'Quote not found'], 404 );
+        $data = json_decode($request->getContent(), true);
+
+        if (!$this->validateRequiredFields($data, ['content', 'author'])) {
+            return $this->json(['error' => 'Missing required fields (content, author)'], Response::HTTP_BAD_REQUEST);
         }
 
-        $data = json_decode( $request->getContent(), true );
+        $quote = (new Quote())
+            ->setContent($data['content'])
+            ->setAuthor($data['author'])
+            ->setIsVerified($data['isVerified'] ?? false);
 
-        // update only if the field is set
-        if ( isset( $data['content'] ) ) {
-            $quote->setContent( $data['content'] );
-        }
-        if ( isset( $data['author'] ) ) {
-            $quote->setAuthor( $data['author'] );
-        }
-        if ( isset( $data['isVerified'] ) ) {
-            $quote->setIsVerified( $data['isVerified'] );
-        }
-        $em->flush();
+        $this->em->persist($quote);
+        $this->em->flush();
 
-        return $this->json( $quote, 200, [], ['groups' => 'quote:read'] );
+        return $this->json($quote, Response::HTTP_CREATED, [], ['groups' => 'quote:read']);
     }
 
-    #[Route( '/{id}', name: 'api_delete_quote', methods: ['DELETE'] )]
-    public function deleteQuote( $id, EntityManagerInterface $em, QuoteRepository $quoteRepository ) : JsonResponse
+    #[Route('/{id}', name: 'api_update_quote', methods: ['PUT'])]
+    public function updateQuote(int $id, Request $request, QuoteRepository $quoteRepository): JsonResponse
     {
-        $quote = $quoteRepository->find( $id );
-        if ( !$quote ) {
-            return $this->json( ['error' => 'Quote not found'], 404 );
+        $quote = $quoteRepository->find($id);
+
+        if (!$quote) {
+            return $this->json(['error' => 'Quote not found'], Response::HTTP_NOT_FOUND);
         }
 
-        $em->remove( $quote );
-        $em->flush();
+        $data = json_decode($request->getContent(), true);
 
-        return $this->json( ['message' => 'Quote deleted'] );
+        $quote
+            ->setContent($data['content'] ?? $quote->getContent())
+            ->setAuthor($data['author'] ?? $quote->getAuthor())
+            ->setIsVerified($data['isVerified'] ?? $quote->getIsVerified());
+
+        $this->em->flush();
+
+        return $this->json($quote, Response::HTTP_OK, [], ['groups' => 'quote:read']);
     }
 
-    #[Route( '/{id}/like', name: 'like', methods: ['POST'] )]
-    public function like( Quote $quote, Request $request, EntityManagerInterface $em, LikeDislikeRepository $likeDislikeRepo ) : JsonResponse
+    #[Route('/{id}', name: 'api_delete_quote', methods: ['DELETE'])]
+    public function deleteQuote(int $id, QuoteRepository $quoteRepository): JsonResponse
+    {
+        $quote = $quoteRepository->find($id);
+
+        if (!$quote) {
+            return $this->json(['error' => 'Quote not found'], Response::HTTP_NOT_FOUND);
+        }
+
+        $this->em->remove($quote);
+        $this->em->flush();
+
+        return $this->json(['message' => 'Quote deleted'], Response::HTTP_OK);
+    }
+
+    #[Route('/{id}/like', name: 'like', methods: ['POST'])]
+    public function like(Quote $quote, Request $request, LikeDislikeRepository $likeDislikeRepo): JsonResponse
+    {
+        return $this->handleVote($quote, $request, $likeDislikeRepo, VoteType::LIKE);
+    }
+
+    #[Route('/{id}/dislike', name: 'dislike', methods: ['POST'])]
+    public function dislike(Quote $quote, Request $request, LikeDislikeRepository $likeDislikeRepo): JsonResponse
+    {
+        return $this->handleVote($quote, $request, $likeDislikeRepo, VoteType::DISLIKE);
+    }
+
+    private function validateRequiredFields(array $data, array $requiredFields): bool
+    {
+        foreach ($requiredFields as $field) {
+            if (!isset($data[$field])) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private function handleVote(Quote $quote, Request $request, LikeDislikeRepository $likeDislikeRepo, VoteType $voteType): JsonResponse
     {
         $ipAddress = $request->getClientIp();
-        $existingVote = $likeDislikeRepo->findOneBy( ['quote' => $quote, 'ipAddress' => $ipAddress] );
+        $existingVote = $likeDislikeRepo->findOneBy(['quote' => $quote, 'ipAddress' => $ipAddress]);
 
-        if ( $existingVote ) {
-            if ( $existingVote->getVoteType() === VoteType::LIKE ) {
-                // Remove like
-                $em->remove( $existingVote );
-                $quote->setLikes( $quote->getLikes() - 1 );
-            } else {
-                // Change dislike to like
-                $existingVote->setVoteType( VoteType::LIKE );
-                $quote->setLikes( $quote->getLikes() + 1 );
-                $quote->setDislikes( $quote->getDislikes() - 1 );
-            }
-        } else {
-            // Add new like
-            $likeDislike = new LikeDislike();
-            $likeDislike->setIpAddress( $ipAddress );
-            $likeDislike->setQuote( $quote );
-            $likeDislike->setVoteType( VoteType::LIKE );
-            $em->persist( $likeDislike );
+        ($existingVote) ? $this->updateExistingVote($existingVote, $quote, $voteType) : $this->addNewVote($ipAddress, $quote, $voteType);
 
-            $quote->setLikes( $quote->getLikes() + 1 );
-        }
+        $this->em->flush();
 
-        $em->flush();
-
-        return $this->json( ['likes' => $quote->getLikes(), 'dislikes' => $quote->getDislikes()], 200, [], ['groups' => 'quote:read'] );
+        return $this->json(['likes' => $quote->getLikes(), 'dislikes' => $quote->getDislikes()], Response::HTTP_OK, [], ['groups' => 'quote:read']);
     }
 
-    #[Route( '/{id}/dislike', name: 'dislike', methods: ['POST'] )]
-    public function dislike( Quote $quote, Request $request, EntityManagerInterface $em, LikeDislikeRepository $likeDislikeRepo ) : JsonResponse
+    private function updateExistingVote(LikeDislike $existingVote, Quote $quote, VoteType $voteType): void
     {
-        $ipAddress = $request->getClientIp();
-        $existingVote = $likeDislikeRepo->findOneBy( ['quote' => $quote, 'ipAddress' => $ipAddress] );
-
-        if ( $existingVote ) {
-            if ( $existingVote->getVoteType() === VoteType::DISLIKE ) {
-                // Remove dislike
-                $em->remove( $existingVote );
-                $quote->setDislikes( $quote->getDislikes() - 1 );
-            } else {
-                // Change like to dislike
-                $existingVote->setVoteType( VoteType::DISLIKE );
-                $quote->setDislikes( $quote->getDislikes() + 1 );
-                $quote->setLikes( $quote->getLikes() - 1 );
-            }
+        if ($existingVote->getVoteType() === $voteType) {
+            $this->em->remove($existingVote);
+            $quote->setLikes($quote->getLikes() - ($voteType === VoteType::LIKE ? 1 : 0));
+            $quote->setDislikes($quote->getDislikes() - ($voteType === VoteType::DISLIKE ? 1 : 0));
         } else {
-            // Add new dislike
-            $likeDislike = new LikeDislike();
-            $likeDislike->setIpAddress( $ipAddress );
-            $likeDislike->setQuote( $quote );
-            $likeDislike->setVoteType( VoteType::DISLIKE );
-            $em->persist( $likeDislike );
-
-            $quote->setDislikes( $quote->getDislikes() + 1 );
+            $existingVote->setVoteType($voteType);
+            $quote->setLikes($quote->getLikes() + ($voteType === VoteType::LIKE ? 1 : -1));
+            $quote->setDislikes($quote->getDislikes() + ($voteType === VoteType::DISLIKE ? 1 : -1));
         }
-
-        $em->flush();
-
-//        return new JsonResponse(['likes' => $quote->getLikes(), 'dislikes' => $quote->getDislikes()]);
-        return $this->json( ['likes' => $quote->getLikes(), 'dislikes' => $quote->getDislikes()], 200, [], ['groups' => 'quote:read'] );
     }
 
+    private function addNewVote(string $ipAddress, Quote $quote, VoteType $voteType): void
+    {
+        $likeDislike = new LikeDislike();
+        $likeDislike->setIpAddress($ipAddress);
+        $likeDislike->setQuote($quote);
+        $likeDislike->setVoteType($voteType);
+
+        $this->em->persist($likeDislike);
+        $quote->setLikes($quote->getLikes() + ($voteType === VoteType::LIKE ? 1 : 0));
+        $quote->setDislikes($quote->getDislikes() + ($voteType === VoteType::DISLIKE ? 1 : 0));
+    }
 }
